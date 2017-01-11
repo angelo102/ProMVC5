13f87af3823b3ed421557921a00bdf6dcabdab3d
angelo102 <angelo102@gmail.com>
2017-01-11 04:16:32 -0600

    Using Object and Collection Initializers

--- PROMVC5_Chapter4/Controllers/HomeController.cs
+++ PROMVC5_Chapter4/Controllers/HomeController.cs
@@ -24,5 +24,30 @@ namespace PROMVC5_Chapter4.Controllers
 
             return View("Result", (object)String.Format("Product Name: {0}", productName));
         }
+
+        public ViewResult CreateProduct()
+        {
+            Product myP = new Product
+            {
+                ProductID = 100,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Price = 275M,
+                Category = "Watersports"
+            };
+
+            return View("Result", (object)String.Format("Category Name: {0}", myP.Category));
+        }
+
+        public ViewResult CreateCollection()
+        {
+            string[] stringArray = { "apple", "orange", "plum" };
+            List<int> intList = new List<int> { 10, 20, 30, 40 };
+            Dictionary<string, int> myDict = new Dictionary<string, int>{
+                {"apple",10},{"orange",20},{"plum",30}
+            };
+
+            return View("Result", (object)stringArray[1]);
+        }
     }
 }
