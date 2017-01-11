bc9857903910a628002580ff39024d96f9a64108
angelo102 <angelo102@gmail.com>
2017-01-11 04:00:10 -0600

    Automatic implemented properties.

---
+++ PROMVC5_Chapter4/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PROMVC5_Chapter4.Models;
+
+namespace PROMVC5_Chapter4.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "navigate to URL to show an example";
+        }
+
+        public ViewResult AutoProperty()
+        {
+            Product myProduct = new Product();
+            myProduct.Name = "Kayak";
+
+            string productName = myProduct.Name;
+
+            return View("Result", (object)String.Format("Product Name: {0}", productName));
+        }
+    }
+}
