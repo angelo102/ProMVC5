744088c45e7209cd33fad7013b2aa12ba8a3fa81
angelo102 <angelo102@gmail.com>
2017-01-18 21:46:55 -0600

    Using Async Methods

---
+++ PROMVC5_Chapter4/Models/MyAsyncMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PROMVC5_Chapter4.Models
+{
+    public  class MyAsyncMethods
+    {
+        public async static Task<long?> GetPageLength()
+        {
+            HttpClient client = new HttpClient();
+            var httpMessage = await client.GetAsync("http://apress.com");
+
+            //Other code here
+
+            return httpMessage.Content.Headers.ContentLength;
+        }
+    }
+}
