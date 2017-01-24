5ceabd016cc3bbf13ff2d5469b55396d0068352c
angelo102@gmail.com <angelo102@gmail.com>
2017-01-23 20:56:43 -0600

    Using Razor Expressions

--- Razor/Controllers/HomeController.cs
+++ Razor/Controllers/HomeController.cs
@@ -28,5 +28,15 @@ namespace Razor.Controllers
             return View(myProduct);
         }
 
+        public ActionResult DemoExpression()
+        {
+            ViewBag.ProductCount = 1;
+            ViewBag.ExpressShip = true;
+            ViewBag.ApplyDiscount = false;
+            ViewBag.Supplier = null;
+
+            return View(myProduct);
+        }
+
     }
 }
