88ab51d5419de9400f22c0459c27f1e6913ab4db
Angel Romero <angelo102@gmail.com>
2017-04-25 18:08:44 -0500

    Ch 7 Adding the HtmlHelper

--- SoprtsStore.UnitTests/UnitTest1.cs
+++ SoprtsStore.UnitTests/UnitTest1.cs
@@ -7,6 +7,9 @@ using SportsStore.WebUI.Controllers;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
 using SportsStore.Domain.Concrete;
+using System.Web.Mvc;
+using SportsStore.WebUI.Models;
+using SportsStore.WebUI.HtmlHelpers;
 
 namespace SportsStore.UnitTests
 {
@@ -42,6 +45,24 @@ namespace SportsStore.UnitTests
             Assert.IsTrue(prodArray.Length == 2);
             Assert.AreEqual(prodArray[0].Name, "P4");
             Assert.AreEqual(prodArray[1].Name, "P5");
+        }
+
+        [TestMethod]
+        public void Can_Generate_Page_Links()
+        {
+            //Arrange
+            HtmlHelper myHelper = null;
+            PagingInfo pagingInfo = new PagingInfo { CurrentPage = 2, TotalItems = 28, ItemsPerPage = 10 };
+            Func<int, string> pageUrlDelegate = i => "Page" + i;
+
+            //Act
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
+
+            //Assert
+            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Page1"">1</a>"
+                    + @"<a class=""btn btn-primary selected"" href=""Page2"">2</a>"
+                    + @"<a class=""btn btn-default"" href=""Page3"">3</a>"
+                    , result.ToString());
         }
     }
 }
