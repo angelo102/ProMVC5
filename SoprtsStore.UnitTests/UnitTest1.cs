258c46efbc127670d6648846bdcc7623182f2c2a
Angel Romero <angelo102@gmail.com>
2017-04-25 17:11:29 -0500

    Ch 7 Adding Pagination

--- SoprtsStore.UnitTests/UnitTest1.cs
+++ SoprtsStore.UnitTests/UnitTest1.cs
@@ -1,5 +1,12 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Collections.Generic;
+using Moq;
+using SportsStore.WebUI.Controllers;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Concrete;
 
 namespace SportsStore.UnitTests
 {
@@ -9,6 +16,32 @@ namespace SportsStore.UnitTests
         [TestMethod]
         public void TestMethod1()
         {
+        }
+
+        [TestMethod]
+        public void CanPaginate()
+        {
+            //Arrange
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product { ProductID = 1, Name = "P1" },
+                new Product { ProductID = 2, Name = "P2" },
+                new Product { ProductID = 3, Name = "P3" },
+                new Product { ProductID = 4, Name = "P4" },
+                new Product { ProductID = 5, Name = "P5" },
+            });
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            //Act
+            IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).Model;
+
+            //Assert
+            Product[] prodArray = result.ToArray();
+            Assert.IsTrue(prodArray.Length == 2);
+            Assert.AreEqual(prodArray[0].Name, "P4");
+            Assert.AreEqual(prodArray[1].Name, "P5");
         }
     }
 }
